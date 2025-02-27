5c14a4a6591c1df121da47894f19fcf0c71fa4eb
Mrityunjay <mrityunjayp1409@gmail.com>
2025-02-27 19:26:53 +0530

    Responsive set

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-console.log("Lets write JavaScript");
 let currentSong = new Audio();
 
-// function secondsToMinutesSeconds(seconds) {
-//   if (isNaN(seconds) || seconds < 0) {
-//     return "Invalid input";
-//   }
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "Invalid input";
+  }
 
-//   const minutes = Math.floor(seconds / 60);
-//   const remainingSeconds = Math.floor(seconds % 60);
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
 
-//   const formattedMinutes = String(minutes).padStart(2, "0");
-//   const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
 
-//   return `${formattedMinutes}:${formattedSeconds}`;
-// }
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getSongs() {
   let a = await fetch(
-    "http://127.0.0.1:8000/project%202%20spotify%20clone/songs/"
+    "http://127.0.0.1:8000/songs/"
   );
   let response = await a.text();
   console.log(response);
@@ -34,15 +33,36 @@ async function getSongs() {
   return songs;
 }
 
-const playMusic = (track, pause = false) => {
-  currentSong.src = "/project 2 spotify clone/songs/" + track;
-  if (!pause) {
-    currentSong.play();
-    play.src = "pause.svg";
+// const playMusic = (track) => {
+//   currentSong.src = "songs/" + track;  // Fix path issue
+//   currentSong.play();
+//   play.src = "pause.svg";  
+
+//   let songInfoElement = document.querySelector(".songinfo");
+//   if (songInfoElement) {
+//     songInfoElement.innerText = track.replaceAll("%20", " ");  // Fix song name update
+//   } else {
+//     console.error("Error: .songinfo element not found!");
+//   }
+
+//   let songTimeElement = document.querySelector(".songtime");
+//   if (songTimeElement) {
+//     songTimeElement.innerText = "00:00 / 00:00";
+//   } else {
+//     console.error("Error: .songtime element not found!");
+//   }
+// };
+
+const playMusic = (track, pause=false) => {
+  currentSong.src = "/songs/" + track
+  if(!pause){
+    currentSong.play()
+    play.src = "pause.svg"
   }
-  // document.querySelector(".songinfo").innerHTML = decodeURI(track);
-  // document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
-};
+  document.querySelector(".songinfo").innerHTML = decodeURI(track)
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
 
 async function main() {
   //Get the list of all the songs
@@ -83,7 +103,39 @@ async function main() {
     }
   })
 
+  // Listen for timeupdate event
+  currentSong.addEventListener("timeupdate", ()=>{
+    console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+    document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration) * 100 + "%";
+  })
 
+  // Add an event listner to seekbar
+  document.querySelector(".seekbar").addEventListener("click", e=>{
+    let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+    currentSong.currentTime = ((currentSong.duration)* percent)/100
+  })
+
+  // Add an event listner for hamburger
+  document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "0"
+  })
+
+  // Add an event listner for close button
+  document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "-120%"
+  })
+
+  // Add an event listner to previous
+  previous.addEventListener("click", ()=>{
+    console.log("Previous clicked")
+  })
+
+  // Add an event listner to next
+  next.addEventListener("click", ()=>{
+    console.log("Next clicked")
+    console.log(currentSong)
+  })
 
 
 }
